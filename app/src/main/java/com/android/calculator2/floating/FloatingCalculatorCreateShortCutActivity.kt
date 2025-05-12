c03ddc1dd61dae6830d97ca2f2aeda777dfee8b9
tuancoltech <tuancoltech@gmail.com>
2025-05-12 21:08:41 +0700

    Convert FloatingCalculatorCreateShortCutActivity class to Kotlin

--- app/src/main/java/com/android/calculator2/floating/FloatingCalculatorCreateShortCutActivity.kt
+++ app/src/main/java/com/android/calculator2/floating/FloatingCalculatorCreateShortCutActivity.kt
@@ -1,37 +1,36 @@
-package com.android.calculator2.floating;
+package com.android.calculator2.floating
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-
-import ai.elimu.calculator.R;
+import ai.elimu.calculator.R
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.ShortcutIconResource
+import android.os.Bundle
 
 /**
  * Created by Will on 4/9/2014.
  */
-public class FloatingCalculatorCreateShortCutActivity extends Activity {
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
+class FloatingCalculatorCreateShortCutActivity : Activity() {
+    public override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
 
-        if(Intent.ACTION_CREATE_SHORTCUT.equals(getIntent().getAction())) {
+        if (Intent.ACTION_CREATE_SHORTCUT == getIntent().getAction()) {
             // create shortcut if requested
-            Intent.ShortcutIconResource icon = Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher_floating);
+            val icon = ShortcutIconResource.fromContext(this, R.drawable.ic_launcher_floating)
 
-            Intent intent = new Intent();
-            Intent launchIntent = new Intent(this, FloatingCalculatorOpenShortCutActivity.class);
+            val intent = Intent()
+            val launchIntent = Intent(this, FloatingCalculatorOpenShortCutActivity::class.java)
 
-            intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent);
-            intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
-            intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+            intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent)
+            intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name))
+            intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon)
 
-            setResult(RESULT_OK, intent);
-            finish();
+            setResult(RESULT_OK, intent)
+            finish()
         }
     }
 
-    @Override
-    public void finish() {
-        super.finish();
-        overridePendingTransition(R.anim.blank, R.anim.blank);
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.blank, R.anim.blank)
     }
 }
