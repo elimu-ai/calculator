8512ab180b09c334773bbdab1189e61f21b66521
tuancoltech <tuancoltech@gmail.com>
2025-05-12 21:09:25 +0700

    Convert FloatingCalculatorOpenShortCutActivity class to Kotlin

--- app/src/main/java/com/android/calculator2/floating/FloatingCalculatorOpenShortCutActivity.kt
+++ app/src/main/java/com/android/calculator2/floating/FloatingCalculatorOpenShortCutActivity.kt
@@ -1,63 +1,63 @@
-package com.android.calculator2.floating;
+package com.android.calculator2.floating
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-
-import android.os.Handler;
-import android.provider.Settings;
-import ai.elimu.calculator.R;
+import ai.elimu.calculator.R
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.provider.Settings
 
 /**
  * Created by Will on 4/9/2014.
  */
-public class FloatingCalculatorOpenShortCutActivity extends Activity {
+class FloatingCalculatorOpenShortCutActivity : Activity() {
+    private val mHandler = Handler()
 
-    private static int REQUEST_SYSTEM_WINDOW_PERMISSION = 69;
-    private Handler mHandler = new Handler();
-
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
+    public override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
 
         if (!Settings.canDrawOverlays(this)) {
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    requestSystemWindowPermission();
+            mHandler.post(object : Runnable {
+                override fun run() {
+                    requestSystemWindowPermission()
                 }
-            });
+            })
         } else {
-            startFloatingCalculator();
+            startFloatingCalculator()
         }
     }
 
-    private void startFloatingCalculator() {
-        Intent intent = new Intent(this, FloatingCalculator.class);
-        startService(intent);
-        finish();
+    private fun startFloatingCalculator() {
+        val intent = Intent(this, FloatingCalculator::class.java)
+        startService(intent)
+        finish()
     }
 
-    @Override
-    public void finish() {
-        super.finish();
-        overridePendingTransition(R.anim.blank, R.anim.blank);
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.blank, R.anim.blank)
     }
 
-    private void requestSystemWindowPermission() {
-        Intent intent = new Intent (Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:" + getPackageName()));
-        startActivityForResult(intent, REQUEST_SYSTEM_WINDOW_PERMISSION);
+    private fun requestSystemWindowPermission() {
+        val intent = Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:" + getPackageName())
+        )
+        startActivityForResult(intent, REQUEST_SYSTEM_WINDOW_PERMISSION)
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_SYSTEM_WINDOW_PERMISSION) {
             if (Settings.canDrawOverlays(this)) {
-                startFloatingCalculator();
+                startFloatingCalculator()
             } else {
-                finish();
+                finish()
             }
         }
+    }
+
+    companion object {
+        private const val REQUEST_SYSTEM_WINDOW_PERMISSION = 69
     }
 }
