34217b80a53177fe4192c87feb1b1791a642397f
tuancoltech <tuancoltech@gmail.com>
2025-05-12 21:20:02 +0700

    Convert AuxPadView class to Kotlin

--- app/src/main/java/com/android/calculator2/view/AuxPadView.kt
+++ app/src/main/java/com/android/calculator2/view/AuxPadView.kt
@@ -1,114 +1,100 @@
-package com.android.calculator2.view;
+package com.android.calculator2.view
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-
-import com.android.calculator2.viewpager.PagerAdapter;
-import com.android.calculator2.viewpager.VerticalViewPager;
-
-import ai.elimu.calculator.R;
+import ai.elimu.calculator.R
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.android.calculator2.viewpager.PagerAdapter
+import com.android.calculator2.viewpager.VerticalViewPager
 
 /**
  * Container for a vertical view pager that adds indicator dots to show the currently
  * selected page.
  */
-public class AuxPadView extends FrameLayout {
+class AuxPadView : FrameLayout {
+    private var mIndicatorView: ViewGroup? = null
 
-    private ViewGroup mIndicatorView;
+    internal class AuxPagerAdapter(private val mViewPager: VerticalViewPager) : PagerAdapter() {
+        override val count: Int
+            get() = mViewPager.getChildCount()
 
-    static class AuxPagerAdapter extends PagerAdapter {
-
-        private VerticalViewPager mViewPager;
-
-        public AuxPagerAdapter(VerticalViewPager viewPager) {
-            mViewPager = viewPager;
+        public override fun instantiateItem(container: ViewGroup?, position: Int): Any? {
+            return mViewPager.getChildAt(position)
         }
 
-        @Override
-        public int getCount() {
-            return mViewPager.getChildCount();
+        public override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
+            mViewPager.removeViewAt(position)
         }
 
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            return mViewPager.getChildAt(position);
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            mViewPager.removeViewAt(position);
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
+        public override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
+            return view === `object`
         }
     }
 
-    class PageChangeListener implements VerticalViewPager.OnPageChangeListener {
-        private int mSelectedIndex = 0;
+    internal inner class PageChangeListener : VerticalViewPager.OnPageChangeListener {
+        private var mSelectedIndex = 0
 
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
         }
 
-        @Override
-        public void onPageSelected(int position) {
-            mIndicatorView.getChildAt(mSelectedIndex).setSelected(false);
-            mIndicatorView.getChildAt(position).setSelected(true);
-            mSelectedIndex = position;
+        override fun onPageSelected(position: Int) {
+            mIndicatorView!!.getChildAt(mSelectedIndex).setSelected(false)
+            mIndicatorView!!.getChildAt(position).setSelected(true)
+            mSelectedIndex = position
         }
 
-        @Override
-        public void onPageScrollStateChanged(int state) {
-
+        override fun onPageScrollStateChanged(state: Int) {
         }
     }
 
-    public AuxPadView(Context context) {
-        super(context);
-    }
+    constructor(context: Context) : super(context)
 
-    public AuxPadView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
-    public AuxPadView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
 
-    public AuxPadView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
 
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
+    override fun onFinishInflate() {
+        super.onFinishInflate()
 
-        VerticalViewPager viewPager = (VerticalViewPager)findViewById(R.id.viewPager);
-        viewPager.setAdapter(new AuxPagerAdapter(viewPager));
-        viewPager.setOnPageChangeListener(new PageChangeListener());
+        val viewPager: VerticalViewPager = findViewById<VerticalViewPager>(R.id.viewPager)
+        viewPager.setAdapter(AuxPagerAdapter(viewPager))
+        viewPager.setOnPageChangeListener(PageChangeListener())
 
-        int margin = getContext().getResources()
-                .getDimensionPixelSize(R.dimen.viewpager_indicator_margin_size);
+        val margin = getContext().getResources()
+            .getDimensionPixelSize(R.dimen.viewpager_indicator_margin_size)
 
-        mIndicatorView = (ViewGroup)findViewById(R.id.pageIndicators);
-        for (int i=0; i < viewPager.getChildCount(); ++i) {
-            ImageView imageView = new ImageView(getContext());
-            imageView.setImageResource(R.drawable.view_pager_indicator);
-            imageView.setSelected(i == 0);
-            LinearLayout.LayoutParams lp =
-                    new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
-                            ViewGroup.LayoutParams.WRAP_CONTENT);
-            lp.setMargins(margin, margin, margin, margin);
-            imageView.setLayoutParams(lp);
-            mIndicatorView.addView(imageView);
+        mIndicatorView = findViewById<ViewGroup>(R.id.pageIndicators)
+        for (i in 0..<viewPager.getChildCount()) {
+            val imageView = ImageView(context)
+            imageView.setImageResource(R.drawable.view_pager_indicator)
+            imageView.setSelected(i == 0)
+            val lp =
+                LinearLayout.LayoutParams(
+                    LayoutParams.WRAP_CONTENT,
+                    LayoutParams.WRAP_CONTENT
+                )
+            lp.setMargins(margin, margin, margin, margin)
+            imageView.setLayoutParams(lp)
+            mIndicatorView!!.addView(imageView)
         }
     }
 }
