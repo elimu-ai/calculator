588495df73faf82e8c51341699bca938b49c3fd3
tuancoltech <tuancoltech@gmail.com>
2025-05-12 21:20:33 +0700

    Convert CalculatorEditable class to Kotlin

--- app/src/main/java/com/android/calculator2/view/CalculatorEditable.kt
+++ app/src/main/java/com/android/calculator2/view/CalculatorEditable.kt
@@ -13,87 +13,91 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.android.calculator2.view
 
-package com.android.calculator2.view;
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import com.xlythe.math.Constants
+import com.xlythe.math.Solver
 
-import android.text.Editable;
-import android.text.SpannableStringBuilder;
+class CalculatorEditable private constructor(source: CharSequence?) :
+    SpannableStringBuilder(source) {
+    private var isInsideReplace = false
 
-import com.xlythe.math.Constants;
-import com.xlythe.math.Solver;
-
-public class CalculatorEditable extends SpannableStringBuilder {
-    private static final char[] ORIGINALS = {'-', '*', '/'};
-    private static final char[] REPLACEMENTS = {'\u2212', '\u00d7', '\u00f7'};
-    private boolean isInsideReplace = false;
-
-    private CalculatorEditable(CharSequence source) {
-        super(source);
-    }
-
-    @Override
-    public SpannableStringBuilder replace(int start, int end, CharSequence tb, int tbstart, int tbend) {
-        if(isInsideReplace) {
-            return super.replace(start, end, tb, tbstart, tbend);
+    override fun replace(
+        start: Int,
+        end: Int,
+        tb: CharSequence,
+        tbstart: Int,
+        tbend: Int
+    ): SpannableStringBuilder? {
+        if (isInsideReplace) {
+            return super.replace(start, end, tb, tbstart, tbend)
         } else {
-            isInsideReplace = true;
+            isInsideReplace = true
             try {
-                String delta = tb.subSequence(tbstart, tbend).toString();
-                return internalReplace(start, end, delta);
+                val delta = tb.subSequence(tbstart, tbend).toString()
+                return internalReplace(start, end, delta)
             } finally {
-                isInsideReplace = false;
+                isInsideReplace = false
             }
         }
     }
 
-    private SpannableStringBuilder internalReplace(int start, int end, String delta) {
-        for(int i = ORIGINALS.length - 1; i >= 0; --i) {
-            delta = delta.replace(ORIGINALS[i], REPLACEMENTS[i]);
+    private fun internalReplace(start: Int, end: Int, delta: String): SpannableStringBuilder? {
+        var start = start
+        var delta = delta
+        for (i in ORIGINALS.indices.reversed()) {
+            delta = delta.replace(ORIGINALS[i], REPLACEMENTS[i])
         }
 
-        int length = delta.length();
-        if(length == 1) {
-            char text = delta.charAt(0);
+        val length = delta.length
+        if (length == 1) {
+            val text = delta.get(0)
 
             // don't allow two dots in the same number
-            if(text == Constants.DECIMAL_POINT) {
-                int p = start - 1;
-                while(p >= 0 && Solver.isDigit(charAt(p))
-                     && (charAt(p) != Constants.DECIMAL_POINT)) {
-                    --p;
+            if (text == Constants.DECIMAL_POINT) {
+                var p = start - 1
+                while (p >= 0 && Solver.isDigit(get(p))
+                    && (get(p) != Constants.DECIMAL_POINT)
+                ) {
+                    --p
                 }
-                if(p >= 0 && charAt(p) == Constants.DECIMAL_POINT) {
-                    return super.replace(start, end, "");
+                if (p >= 0 && get(p) == Constants.DECIMAL_POINT) {
+                    return super.replace(start, end, "")
                 }
             }
 
-            char prevChar = start > 0 ? charAt(start - 1) : '\0';
+            var prevChar = if (start > 0) get(start - 1) else '\u0000'
 
             // don't allow 2 successive minuses
-            if(text == Constants.MINUS && prevChar == Constants.MINUS) {
-                return super.replace(start, end, "");
+            if (text == Constants.MINUS && prevChar == Constants.MINUS) {
+                return super.replace(start, end, "")
             }
 
             // don't allow multiple successive operators unless we're dealing with negatives
-            if(Solver.isOperator(text) && text != Constants.MINUS) {
-                while(Solver.isOperator(prevChar)) {
-                    if(start == 1) {
-                        return super.replace(start, end, "");
+            if (Solver.isOperator(text) && text != Constants.MINUS) {
+                while (Solver.isOperator(prevChar)) {
+                    if (start == 1) {
+                        return super.replace(start, end, "")
                     }
 
-                    --start;
-                    prevChar = start > 0 ? charAt(start - 1) : '\0';
+                    --start
+                    prevChar = if (start > 0) get(start - 1) else '\u0000'
                 }
             }
         }
-        return super.replace(start, end, delta);
+        return super.replace(start, end, delta)
     }
 
-    public static class Factory extends Editable.Factory {
-        public Factory() {}
-
-        public Editable newEditable(CharSequence source) {
-            return new CalculatorEditable(source);
+    class Factory : Editable.Factory() {
+        override fun newEditable(source: CharSequence?): Editable {
+            return CalculatorEditable(source)
         }
+    }
+
+    companion object {
+        private val ORIGINALS = charArrayOf('-', '*', '/')
+        private val REPLACEMENTS = charArrayOf('\u2212', '\u00d7', '\u00f7')
     }
 }
