53a492635339aa5a73dfb15bdd4502f306b4d1d9
tuancoltech <tuancoltech@gmail.com>
2025-05-12 21:17:02 +0700

    Convert PlayerUtil class to Kotlin

--- app/src/main/java/com/android/calculator2/util/PlayerUtil.kt
+++ app/src/main/java/com/android/calculator2/util/PlayerUtil.kt
@@ -1,47 +1,47 @@
-package com.android.calculator2.util;
+package com.android.calculator2.util
 
-import android.content.Context;
-import android.media.MediaPlayer;
+import android.content.Context
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnCompletionListener
 
 /**
  * Created by GSC on 12/11/2016.
  */
-public class PlayerUtil {
+object PlayerUtil {
+    var RAW_FILE_EQUALS: String = "equals"
 
-    public static String RAW_FILE_EQUALS = "equals";
+    val NUMBER_RAW_FILES: Array<String> = arrayOf<String>(
+        "digit_0",
+        "digit_1",
+        "digit_2",
+        "digit_3",
+        "digit_4",
+        "digit_5",
+        "digit_6",
+        "digit_7",
+        "digit_8",
+        "digit_9"
+    )
 
-    public static final String[] NUMBER_RAW_FILES = new String[] {
-            "digit_0",
-            "digit_1",
-            "digit_2",
-            "digit_3",
-            "digit_4",
-            "digit_5",
-            "digit_6",
-            "digit_7",
-            "digit_8",
-            "digit_9"
-    };
-
-    public static void playRawFile(Context context, String rawFile) {
-        int rawId = context.getResources().getIdentifier(rawFile, "raw", context.getPackageName());
+    fun playRawFile(context: Context, rawFile: String?) {
+        val rawId = context.getResources().getIdentifier(rawFile, "raw", context.getPackageName())
         if (rawId != 0) {
-            MediaPlayer mediaPlayer = MediaPlayer.create(context, rawId);
-            mediaPlayer.start();
+            val mediaPlayer = MediaPlayer.create(context, rawId)
+            mediaPlayer.start()
         }
     }
 
-    public static void playResult(final Context context, final String result) {
-        int rawId = context.getResources().getIdentifier(RAW_FILE_EQUALS, "raw", context.getPackageName());
+    fun playResult(context: Context, result: String?) {
+        val rawId =
+            context.getResources().getIdentifier(RAW_FILE_EQUALS, "raw", context.getPackageName())
         if (rawId != 0) {
-            final MediaPlayer mediaPlayer = MediaPlayer.create(context, rawId);
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    playRawFile(context, result);
+            val mediaPlayer = MediaPlayer.create(context, rawId)
+            mediaPlayer.setOnCompletionListener(object : OnCompletionListener {
+                override fun onCompletion(mp: MediaPlayer?) {
+                    playRawFile(context, result)
                 }
-            });
-            mediaPlayer.start();
+            })
+            mediaPlayer.start()
         }
     }
 }
