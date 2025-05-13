d7564bdea4a7fd5e06c64c669f82e014750487c8
tuancoltech <tuancoltech@gmail.com>
2025-05-13 12:49:51 +0700

    Convert HistoryLine to Kotlin

--- app/src/main/java/com/android/calculator2/view/HistoryLine.kt
+++ app/src/main/java/com/android/calculator2/view/HistoryLine.kt
@@ -1,114 +1,108 @@
-package com.android.calculator2.view;
+package com.android.calculator2.view
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.content.res.Resources;
-import androidx.recyclerview.widget.RecyclerView;
-import android.text.TextUtils;
-import android.util.AttributeSet;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import ai.elimu.calculator.R
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.xlythe.math.History
+import com.xlythe.math.HistoryEntry
 
-import ai.elimu.calculator.R;
-import com.xlythe.math.History;
-import com.xlythe.math.HistoryEntry;
+class HistoryLine(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+    private var mMenuItemsStrings: Array<String>? = null
+    private var mHistoryEntry: HistoryEntry? = null
+    private var mHistory: History? = null
+    private var mAdapter: RecyclerView.Adapter<*>? = null
 
-public class HistoryLine extends LinearLayout {
-    private static final int COPY = 0;
-    private static final int COPY_BASE = 1;
-    private static final int COPY_EDITED = 2;
-    private static final int REMOVE = 3;
-    private String[] mMenuItemsStrings;
-    private HistoryEntry mHistoryEntry;
-    private History mHistory;
-    private RecyclerView.Adapter mAdapter;
-
-    public HistoryLine(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu) {
-        MenuHandler handler = new MenuHandler();
-        if(mMenuItemsStrings == null) {
-            Resources resources = getResources();
-            mMenuItemsStrings = new String[4];
-            mMenuItemsStrings[COPY] = String.format(resources.getString(R.string.copy), mHistoryEntry.getBase() + "=" + mHistoryEntry.getEdited());
-            mMenuItemsStrings[COPY_BASE] = String.format(resources.getString(R.string.copy), mHistoryEntry.getBase());
-            mMenuItemsStrings[COPY_EDITED] = String.format(resources.getString(R.string.copy), mHistoryEntry.getEdited());
-            mMenuItemsStrings[REMOVE] = resources.getString(R.string.remove_from_history);
+    public override fun onCreateContextMenu(menu: ContextMenu) {
+        val handler: MenuHandler = MenuHandler()
+        if (mMenuItemsStrings == null) {
+            val resources = getResources()
+            mMenuItemsStrings = arrayOfNulls<String>(4) as Array<String>?
+            mMenuItemsStrings!![COPY] = String.format(
+                resources.getString(R.string.copy),
+                mHistoryEntry!!.base + "=" + mHistoryEntry!!.edited
+            )
+            mMenuItemsStrings!![COPY_BASE] =
+                String.format(resources.getString(R.string.copy), mHistoryEntry!!.base)
+            mMenuItemsStrings!![COPY_EDITED] =
+                String.format(resources.getString(R.string.copy), mHistoryEntry!!.edited)
+            mMenuItemsStrings!![REMOVE] = resources.getString(R.string.remove_from_history)
         }
-        for(int i = 0; i < mMenuItemsStrings.length; i++) {
-            menu.add(Menu.NONE, i, i, mMenuItemsStrings[i]).setOnMenuItemClickListener(handler);
+        for (i in mMenuItemsStrings!!.indices) {
+            menu.add(Menu.NONE, i, i, mMenuItemsStrings!![i]).setOnMenuItemClickListener(handler)
         }
     }
 
-    public boolean onTextContextMenuItem(CharSequence title) {
-        boolean handled = false;
-        if(TextUtils.equals(title, mMenuItemsStrings[COPY])) {
-            copyContent(mHistoryEntry.getBase() + "=" + mHistoryEntry.getEdited());
-            handled = true;
-        } else if(TextUtils.equals(title, mMenuItemsStrings[COPY_BASE])) {
-            copyContent(mHistoryEntry.getBase());
-            handled = true;
-        } else if(TextUtils.equals(title, mMenuItemsStrings[COPY_EDITED])) {
-            copyContent(mHistoryEntry.getEdited());
-            handled = true;
-        } else if(TextUtils.equals(title, mMenuItemsStrings[REMOVE])) {
-            removeContent();
-            handled = true;
+    fun onTextContextMenuItem(title: CharSequence?): Boolean {
+        var handled = false
+        if (TextUtils.equals(title, mMenuItemsStrings!![COPY])) {
+            copyContent(mHistoryEntry!!.base + "=" + mHistoryEntry!!.edited)
+            handled = true
+        } else if (TextUtils.equals(title, mMenuItemsStrings!![COPY_BASE])) {
+            copyContent(mHistoryEntry!!.base)
+            handled = true
+        } else if (TextUtils.equals(title, mMenuItemsStrings!![COPY_EDITED])) {
+            copyContent(mHistoryEntry!!.edited!!)
+            handled = true
+        } else if (TextUtils.equals(title, mMenuItemsStrings!![REMOVE])) {
+            removeContent()
+            handled = true
         }
-        return handled;
+        return handled
     }
 
-    public void copyContent(String content) {
-        ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-        clipboard.setPrimaryClip(ClipData.newPlainText(null, content));
-        String toastText = String.format(getResources().getString(R.string.text_copied_toast), content);
-        Toast.makeText(getContext(), toastText, Toast.LENGTH_SHORT).show();
+    fun copyContent(content: String) {
+        val clipboard = getContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.setPrimaryClip(ClipData.newPlainText(null, content))
+        val toastText = String.format(getResources().getString(R.string.text_copied_toast), content)
+        Toast.makeText(getContext(), toastText, Toast.LENGTH_SHORT).show()
     }
 
-    private void removeContent() {
-        mHistory.remove(mHistoryEntry);
-        mAdapter.notifyDataSetChanged();
+    private fun removeContent() {
+        mHistory!!.remove(mHistoryEntry)
+        mAdapter!!.notifyDataSetChanged()
     }
 
-    public HistoryEntry getHistoryEntry() {
-        return mHistoryEntry;
-    }
-
-    public void setHistoryEntry(HistoryEntry historyEntry) {
-        this.mHistoryEntry = historyEntry;
-    }
-
-    public History getHistory() {
-        return mHistory;
-    }
-
-    public void setHistory(History history) {
-        this.mHistory = history;
-    }
-
-    public RecyclerView.Adapter getAdapter() {
-        return mAdapter;
-    }
-
-    public void setAdapter(RecyclerView.Adapter adapter) {
-        this.mAdapter = adapter;
-    }
-
-    public void showMenu() {
-        showContextMenu();
-    }
-
-    private class MenuHandler implements MenuItem.OnMenuItemClickListener {
-        public boolean onMenuItemClick(MenuItem item) {
-            return onTextContextMenuItem(item.getTitle());
+    var historyEntry: HistoryEntry
+        get() = mHistoryEntry!!
+        set(historyEntry) {
+            this.mHistoryEntry = historyEntry
         }
+
+    var history: History?
+        get() = mHistory
+        set(history) {
+            this.mHistory = history
+        }
+
+    var adapter: RecyclerView.Adapter<*>
+        get() = mAdapter!!
+        set(adapter) {
+            this.mAdapter = adapter
+        }
+
+    fun showMenu() {
+        showContextMenu()
+    }
+
+    private inner class MenuHandler : MenuItem.OnMenuItemClickListener {
+        override fun onMenuItemClick(item: MenuItem): Boolean {
+            return onTextContextMenuItem(item.getTitle())
+        }
+    }
+
+    companion object {
+        private const val COPY = 0
+        private const val COPY_BASE = 1
+        private const val COPY_EDITED = 2
+        private const val REMOVE = 3
     }
 }
